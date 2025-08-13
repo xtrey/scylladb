2b5e81f978b1aa487495663b2b7dd4dee5bb5f7a
Evgeniy Naydanov <evgeniy.naydanov@scylladb.com>
2025-08-13 09:43:07 +0000

    test.py: switch Python/Approval/Topology tests to pytest
    Switch Python and CQL Approval tests from test.py to pytest:
    
    1. Rename suite.yaml -> test_config.yaml
    2. Add 2 new groups to PYTEST_RUNNER_DIRECTORIES: `python` and `cluster`
    3. For cqlpy change type to Python and remove suite/cql_approval.py file
    
    Additional changes:
    
    - add `--test-py-init` argument for pytest run via run_pytest() function
    - add support for test_config.yaml file as a suite config
    - use TEST_DIR constant for test.py test discovery

--- test.py
+++ test.py
@@ -19,7 +19,6 @@ import pytest
 from types import SimpleNamespace
 
 import colorama
-import glob
 import itertools
 import logging
 import multiprocessing
@@ -64,6 +63,24 @@ PYTEST_RUNNER_DIRECTORIES = [
             TEST_DIR / "ldap",
             TEST_DIR / "raft",
             TEST_DIR / "unit",
+        ],
+    },
+    {
+        "group": "python",
+        "concurrency": lambda jobs: jobs * 2,
+        "dirs": [
+            TEST_DIR / "alternator",
+            TEST_DIR / "broadcast_tables",
+            TEST_DIR / "cql",
+            TEST_DIR / "cqlpy",
+            TEST_DIR / "rest_api",
+        ],
+    },
+    {
+        "group": "cluster",
+        "concurrency": lambda jobs: jobs // 2,
+        "dirs": [
+            TEST_DIR / "cluster",
         ],
     },
 ]
@@ -299,11 +316,11 @@ def parse_cmd_line() -> argparse.Namespace:
 
 
 async def find_tests(options: argparse.Namespace) -> None:
-
-    for f in glob.glob(os.path.join("test", "*")):
-        if os.path.isdir(f) and os.path.isfile(os.path.join(f, SUITE_CONFIG_FILENAME)):
+    for f in TEST_DIR.glob("*"):
+        config = pathlib.Path(f) / SUITE_CONFIG_FILENAME
+        if config.is_file():
             for mode in options.modes:
-                suite = TestSuite.opt_create(f, options, mode)
+                suite = TestSuite.opt_create(config=config, options=options, mode=mode)
                 await suite.add_test_list()
 
 
