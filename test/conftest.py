371c6908d8b14e135698cf6c211b73485995ca68
Andrei Chekun <andrei.chekun@scylladb.com>
2026-03-11 13:53:48 +0100

    test.py: remove testpy_test_fixture_scope
    With migration to pyest this fixture is useless. Removing and setting
    the session to the module for the most of the tests.
    Add dynamic_scope function to support running alternator fixtures in
    session scope, while Test and TestSuite are not deleted. This is for
    migration period, later on this function should be deleted.

--- test/conftest.py
+++ test/conftest.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
 #
-
+import _pytest
 import pytest
 
 from test import TEST_RUNNER
@@ -11,6 +11,16 @@ from test.pylib.report_plugin import ReportPlugin
 
 
 pytest_plugins = []
+
+def dynamic_scope() -> _pytest.scope._ScopeName:
+    """Dynamic scope for fixtures which rely on a current test.py suite/test.
+
+    Even though test.py not running tests anymore, there is some logic still there that requires module scope.
+    When using runpy runner, all custom logic should be disabled and scope should be session.
+    """
+    if TEST_RUNNER == "runpy":
+        return "session"
+    return "module"
 
 
 if TEST_RUNNER == "runpy":
