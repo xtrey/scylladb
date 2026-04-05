170c71b25dfdcb3a785583b69ecd4e5cbb394f9d
Avi Kivity <avi@scylladb.com>
2026-04-05 17:30:09 +0300

    auth_test: coroutinize role_permissions_table_is_protected
    Use co_await for improved readability.

--- test/boost/auth_test.cc
+++ test/boost/auth_test.cc
@@ -170,7 +170,7 @@ SEASTAR_TEST_CASE(role_members_table_is_protected) {
 }
 
 SEASTAR_TEST_CASE(role_permissions_table_is_protected) {
-    return do_with_cql_env_thread([] (cql_test_env& env) {
+    co_await do_with_cql_env_thread([] (cql_test_env& env) {
         require_table_protected(env, "system.role_permissions");
     }, auth_on());
 }
