4eeb5ef54d30d3f4944126b38788b7f9caa67161
Avi Kivity <avi@scylladb.com>
2026-04-05 17:31:02 +0300

    auth_test: coroutinize test_alter_with_timeouts
    Use co_await instead of return for improved readability.

--- test/boost/auth_test.cc
+++ test/boost/auth_test.cc
@@ -176,7 +176,7 @@ SEASTAR_TEST_CASE(role_permissions_table_is_protected) {
 }
 
 SEASTAR_TEST_CASE(test_alter_with_timeouts) {
-    return do_with_cql_env_thread([] (cql_test_env& e) {
+    co_await do_with_cql_env_thread([] (cql_test_env& e) {
         cquery_nofail(e, "CREATE ROLE user1 WITH PASSWORD = 'pass' AND LOGIN = true");
         cquery_nofail(e, "CREATE ROLE user2 WITH PASSWORD = 'pass' AND LOGIN = true");
         cquery_nofail(e, "CREATE ROLE user3 WITH PASSWORD = 'pass' AND LOGIN = true");
